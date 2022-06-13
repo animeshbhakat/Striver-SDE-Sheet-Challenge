9ebe7cc0d6b5fafb359bc5f23c1af99c23ba2a30
Animesh Bhakat <60314168+animeshbhakat@users.noreply.github.com>
2022-06-13 12:43:45 +0530

    Added add two numbers as linked lists solution

---
+++ AddTwoNumbersAsLinkedLists.java
@@ -0,0 +1,44 @@
+import java.util.* ;
+import java.io.*; 
+/****************************************************************
+
+    Following is the class structure of the Node class:
+
+    class LinkedListNode {
+        int data;
+        LinkedListNode next;
+        
+        public LinkedListNode(int data) {
+            this.data = data;
+        }
+    }
+
+*****************************************************************/
+
+
+public class Solution {
+    static LinkedListNode addTwoNumbers(LinkedListNode head1, LinkedListNode head2) {
+        LinkedListNode temp = new LinkedListNode(0);
+        LinkedListNode dummy = temp;
+        int carry = 0;
+        while(head1 != null || head2 != null || carry != 0){
+            int sum = 0;
+            if(head1 != null){
+                sum += head1.data;
+                head1 = head1.next;
+            }
+            if(head2 != null){
+                sum += head2.data;
+                head2 = head2.next;
+            }
+            if(carry != 0){
+                sum += carry;
+            }
+            carry = sum / 10;
+            LinkedListNode node = new LinkedListNode(sum % 10);
+            dummy.next = node;
+            dummy = dummy.next;
+        }
+        return temp.next;
+    }
+}
